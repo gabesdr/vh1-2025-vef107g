c7d4fe7ba31b65e6592fdb12db99e59a509d4c4d
gabesdr <gabbidr00.03@gmail.com>
2025-10-16 15:13:57 +0000

    scriptssoftpatch

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
 
 // efni/xxx.html og styles/header.scss
 
-// Keyrir kóði sem togglar bara á mobile view 768px
+//Kóði sem togglar bara á mobile view 768px
 document.addEventListener('DOMContentLoaded', () => {
     const navToggle = document.querySelector('.nav-toggle');
     const navClose = document.querySelector('.nav-close');
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
         navToggle.setAttribute('aria-label', isOpen ? 'Loka valmynd' : 'Opna valmynd');
         mobileMenu.hidden = !isOpen; // Hidden Attribute
     }
-    // Click-Event sem keyrir toggle-menu button í html.
+    // Click-Event sem stjórnar toggle-menu button í html.
     navToggle.addEventListener('click', toggleMenu);
 
     if (navClose) {
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
             mobileMenu.hidden = true;
         });
     });
-    // "Escape" key handler ef notandi er með lyklaborð á Spjaldtölvu.
+    // "Escape" key handler ef Notandinn er með lyklaborð á Spjaldtölvu.
     document.addEventListener('keydown', (e) => {
         if (e.key === 'Escape' && body.classList.contains('is-menu-open')) {
             body.classList.remove('is-menu-open');
